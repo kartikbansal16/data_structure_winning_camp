3532d0033c1e06156976f7fd4d39c53c8d9745f0
kartikbansal16 <104322752+kartikbansal16@users.noreply.github.com>
2023-01-12 11:51:09 +0530

    Add files via upload

---
+++ LinnkedList_palindrome.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        Node *head = inputTillNegativeOne();
+        bool flag=ispalindrome(head);
+        if (flag)
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+        // cout << "endl";
+    }
+    return 0;
+}
